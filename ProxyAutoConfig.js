62658c6539553edd434eb9fc12136aace0972fb0
hstp <6bxy0t4v@yopmail.com>
2017-01-14 01:47:37 +0700

    Add files via upload

--- ProxyAutoConfig.js
+++ ProxyAutoConfig.js
@@ -6,8 +6,8 @@ var PROXY = 'PROXY 123.30.238.16:3128;';
 //SOCKS = DIRECT; 
 //PROXY = DIRECT;
 
-var WHITE_LIST_SOCKS = ['.googlevideo.com','.hdonline.vn','.ytimg.com', 'img.hayhaytv.vn','t.hdviet.com'];
-var WHITE_LIST_PROXY = ['hdonline.vn', 'youtube.com', 'youtu.be','www.hayhaytv.vn', 'movies.hdviet.com'];
+var WHITE_LIST_SOCKS = ['.googlevideo.com','.hdonline.vn','.ytimg.com', 'img.hayhaytv.vn'];
+var WHITE_LIST_PROXY = ['hdonline.vn', 'youtube.com', 'youtu.be','www.hayhaytv.vn', 'hdviet.com'];
 
 var IsURLAlowed = function(host, white_list) {
   return white_list.some(function(element) {
