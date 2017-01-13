f1586469bb65a64cf895ea1cc866cf30a6e235f6
hstp <6bxy0t4v@yopmail.com>
2017-01-14 01:33:01 +0700

    Add files via upload

---
+++ ProxyAutoConfig.js
@@ -0,0 +1,27 @@
+var DIRECT = 'DIRECT;';
+var DENINE = 'PROXY localhost:0;';
+var SOCKS = 'SOCKS localhost:1080;' + DIRECT;
+var PROXY = 'PROXY 123.30.238.16:3128;';
+
+//SOCKS = DIRECT; 
+//PROXY = DIRECT;
+
+var WHITE_LIST_SOCKS = ['.googlevideo.com','.hdonline.vn','.ytimg.com', 'img.hayhaytv.vn','t.hdviet.com'];
+var WHITE_LIST_PROXY = ['hdonline.vn', 'youtube.com', 'youtu.be','www.hayhaytv.vn', 'movies.hdviet.com'];
+
+var IsURLAlowed = function(host, white_list) {
+  return white_list.some(function(element) {
+    return dnsDomainIs(host, element);
+  });
+}
+
+var FindProxyForURL = function(url, host) {
+  if (dnsResolve(host)) {
+    if (IsURLAlowed(host, WHITE_LIST_SOCKS)) {
+      return SOCKS;
+    } else if (IsURLAlowed(host, WHITE_LIST_PROXY)) {
+      return PROXY;
+    }
+  }
+  return DENINE;
+}
