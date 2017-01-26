47e113d693c4c09733b7c3d7c5eb53da699bf8f7
hstp <6bxy0t4v@yopmail.com>
2017-01-26 20:04:30 +0700

    Add files via upload

--- ProxyAutoConfig.js
+++ ProxyAutoConfig.js
@@ -4,13 +4,11 @@ var DENINE = 'PROXY localhost:0;';
 var SOCKS = 'SOCKS localhost:1080;' + DIRECT;
 var PROXY = 'PROXY 123.30.238.16:3128;';
 
-//SOCKS = DIRECT; 
-//PROXY = DIRECT;
+SOCKS = DIRECT; 
+PROXY = DIRECT;
 
-var WHITE_LIST_SOCKS = ['.googlevideo.com','.hdonline.vn','.ytimg.com', '.highwebmedia.com'];
-var WHITE_LIST_PROXY = ['hdonline.vn', 'youtube.com', 'youtu.be', 'freelivesex.me','.chaturbate.com','gstatic.com',
-'google.com'
-];
+var WHITE_LIST_SOCKS = ['.googlevideo.com','.hdonline.vn','.ytimg.com'];
+var WHITE_LIST_PROXY = ['hdonline.vn', 'youtube.com', 'youtu.be'];
 
 var IsURLAlowed = function(host, white_list) {
   return white_list.some(function(element) {
