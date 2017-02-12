ba86714cf1b69898fe63dd8c0f6a8d3ffa1b291c
hstp <6bxy0t4v@yopmail.com>
2017-02-12 14:23:07 +0700

    Add files via upload

--- ProxyAutoConfig.js
+++ ProxyAutoConfig.js
@@ -3,18 +3,19 @@
 var DIRECT = 'DIRECT;';
 var DENINE = 'PROXY localhost:0;';
 
-var WHITE_LIST = ['hdonline.vn', 'youtube.com', 'youtu.be', 'fptplay.net','.googlevideo.com','.ytimg.com'];
+var WHITE_LIST = ['hdonline.vn', 'youtube.com', 'youtu.be', 'fptplay.net', '.googlevideo.com', '.ytimg.com'];
 
-var IsURLAlowed = function(host, white_list) {
-  return white_list.some(function(element) {
+var IsURLAlowed = function (host, white_list) {
+  return white_list.some(function (element) {
     return dnsDomainIs(host, element);
   });
 }
 
-var FindProxyForURL = function(url, host) {
+var FindProxyForURL = function (url, host) {
   if (dnsResolve(host)) {
     if (IsURLAlowed(host, WHITE_LIST)) {
       return DIRECT;
-    }   }
+    }
+  }
   return DENINE;
 }
